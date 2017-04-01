531b9ed00c013c4776e3df2dedf821dd5bf3b26b
popovsergiy <popow.sergiy@gmail.com>
2017-04-01 19:40:19 +0300

    Add translation files

--- app/code/local/Agere/PreOrder/Helper/Data.php
+++ app/code/local/Agere/PreOrder/Helper/Data.php
@@ -14,15 +14,34 @@ class Agere_PreOrder_Helper_Data extends Mage_Core_Helper_Abstract
 
     public function getAddToCartTemplate()
     {
+
+        //Mage::app()->getLayout()->createBlock('page/html_header');
+        //$this->getLayout()->getBlock('root')->getTemplate();
+        //product.info.addtocart
+
+        //Mage::app()->getLayout()->createBlock('page/html_header');
+        //$root = Mage::app()->getLayout()->getBlock('root')->getTemplate();
+        //$addtocart = Mage::app()->getLayout()->getBlock('product.info.addtocart')->getTemplate();
+        //product.info.addtocart
+
+
         $mode = Mage::getStoreConfig('agere_preOrder/settings/mode');
+        $allowCategoryIds = explode(',', Mage::getStoreConfig('agere_preOrder/settings/categories'));
+
+        $product = Mage::registry('current_product');
+        $categoryIds = $product->getCategoryIds();
 
         $template = '';
-        if ('simple' === $mode) {
-            $template = 'agere/catalog/product/view/addtocart.phtml';
-        } elseif ('advanced' === $mode) {
-            Mage::throwException(sprintf('Mode "%s" not yet implemented', $mode));
+        if (array_intersect($allowCategoryIds, $categoryIds)) {
+            if ('simple' === $mode) {
+                $template = 'agere/catalog/product/view/addtocart.phtml';
+            } elseif ('advanced' === $mode) {
+                Mage::throwException(sprintf('Mode "%s" not yet implemented', $mode));
+            }
         } else {
-            Mage::throwException(sprintf('Unresolved "%s" mode', $mode));
+            //Mage::throwException(sprintf('Unresolved "%s" mode', $mode));
+            // return default value from layout
+            $template = Mage::app()->getLayout()->getBlock('product.info.addtocart')->getTemplate();
         }
 
         return $template;
